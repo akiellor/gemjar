175f364d70f38a7b866bc18ab4a4cfafde198bee
Andrew Kiellor <akiellor@thoughtworks.com>
2012-07-07 17:24:43 -0700

    Fix acceptance tests.

--- acceptance-tests/src/main/features/step_definitions/env.rb
+++ acceptance-tests/src/main/features/step_definitions/env.rb
@@ -1,6 +1,7 @@
 require 'java'
 require 'tmpdir'
 require 'rspec/expectations'
+require 'net/http'
 
 module Acceptance
   class Configuration
@@ -10,6 +11,22 @@ module Acceptance
 
     def self.work_directory
       @work ||= Dir.mktmpdir
+    end
+  end
+end
+
+AfterConfiguration do
+  IO.popen("java -jar #{Java::JavaLang::System.get_property("target.application")}")
+
+  Timeout::timeout(360) do
+    up = false
+    until up
+      begin
+        res = Net::HTTP.get_response(URI.parse("#{Acceptance::Configuration.server}/ping"))
+        up = res.code == "200"
+      rescue Errno::ECONNREFUSED => e
+      end
+      sleep 10
     end
   end
 end
