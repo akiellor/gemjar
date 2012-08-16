b7f3221e401a28f7b322d7d4def4de97dbb364dd
Andrew Kiellor <akiellor@thoughtworks.com>
2012-08-15 20:24:56 -0700

    Moved Artifact.install to the ArtifactRepository.

--- webapp/src/main/ruby/gemjar/artifact.rb
+++ webapp/src/main/ruby/gemjar/artifact.rb
@@ -1,14 +1,9 @@
-require 'gemjar/gem'
-require 'gemjar/logger'
-require 'gemjar/artifact_builder'
 require 'gemjar/artifact_repository'
-require 'gemjar/task_executor'
+require 'gemjar/logger'
 
 module Gemjar
   class Artifact < Struct.new(:jar, :ivy)
     include Gemjar::Logger
-
-    TASK_EXECUTOR = Gemjar::TaskExecutor.new 10
 
     def self.ensure name, version
       log.info("Ensuring artifact #{name}-#{version} is installed.")
@@ -20,21 +15,7 @@ module Gemjar
     end
 
     def self.install name, version
-      future = TASK_EXECUTOR.get_or_submit_task "#{name}-#{version}" do
-        begin
-          log.info("Installing artifact: '#{name}-#{version}'")
-          gem = Gem.install(name, version)
-
-          ArtifactBuilder.new(Gemjar::WORK_DIRECTORY).build(gem).tap do |a|
-            log.info("Retrieved artifact: #{a.inspect}")
-          end
-        rescue => e
-          log.error("Failed to retrieve artifact: #{name}-#{version}'", e)
-          nil
-        end
-      end
-
-      future.get
+      ArtifactRepository.new(Gemjar::WORK_DIRECTORY).install name, version
     end
   end
 end
--- webapp/src/main/ruby/gemjar/artifact_repository.rb
+++ webapp/src/main/ruby/gemjar/artifact_repository.rb
@@ -1,8 +1,13 @@
+require 'gemjar/artifact'
+require 'gemjar/artifact_builder'
 require 'gemjar/logger'
+require 'gemjar/task_executor'
 
 module Gemjar
   class ArtifactRepository
     include Gemjar::Logger
+
+    TASK_EXECUTOR = Gemjar::TaskExecutor.new 10
 
     def initialize directory
       @directory = directory
@@ -14,6 +19,24 @@ module Gemjar
       if File.exists?(jar) && File.exists?(ivy)
         Artifact.new(jar, ivy).tap {|a| log.info("Found artifact #{name}-#{version}: #{a.inspect}")}
       end
+    end
+
+    def install name, version
+      future = TASK_EXECUTOR.get_or_submit_task "#{name}-#{version}" do
+        begin
+          log.info("Installing artifact: '#{name}-#{version}'")
+          gem = Gem.install(name, version)
+
+          ArtifactBuilder.new(Gemjar::WORK_DIRECTORY).build(gem).tap do |a|
+            log.info("Retrieved artifact: #{a.inspect}")
+          end
+        rescue => e
+          log.error("Failed to retrieve artifact: #{name}-#{version}'", e)
+          nil
+        end
+      end
+
+      future.get
     end
   end
 end
