c48b586a16f007a5ac64989c33e1af5a0a2c9e11
Andrew Kiellor <akiellor@gmail.com>
2013-01-15 09:06:41 -0800

    ArtifactPaths is responsible for opening files.

--- webapp/src/main/ruby/gemjar/artifact_builder.rb
+++ webapp/src/main/ruby/gemjar/artifact_builder.rb
@@ -1,7 +1,5 @@
 require 'gemjar/artifact'
 require 'gemjar/artifact_paths'
-require 'zip/zip'
-require 'zip/zipfilesystem'
 require 'fileutils'
 
 module Gemjar
@@ -12,16 +10,13 @@ module Gemjar
 
     def build gem
       paths = ArtifactPaths.new @directory, gem.name, gem.version
-      File.open(paths.ivy, 'w+') { |f| f.write(gem.ivy_module_xml) }
-
-      File.open(paths.pom, 'w+') { |f| f.write(gem.pom_xml) }
-
       FileUtils.rm_rf [File.expand_path("cache", gem.installed_dir)]
-
       FileUtils.rm paths.jar, :force => true
 
-      Zip::ZipFile.open(paths.jar, 'w') do |zipfile|
-        Dir["#{gem.installed_dir}/**/**"].reject { |f| f== paths.jar }.each do |file|
+      paths.open_ivy {|i| i.write gem.ivy_module_xml }
+      paths.open_pom { |p| p.write(gem.pom_xml) }
+      paths.open_jar do |zipfile|
+        Dir["#{gem.installed_dir}/**/**"].each do |file|
           zipfile.add(file.sub(gem.installed_dir+'/', ''), file)
         end
       end
--- webapp/src/main/ruby/gemjar/artifact_paths.rb
+++ webapp/src/main/ruby/gemjar/artifact_paths.rb
@@ -1,3 +1,6 @@
+require 'zip/zip'
+require 'zip/zipfilesystem'
+
 module Gemjar
   class ArtifactPaths
     attr_reader :jar, :ivy, :pom
@@ -6,6 +9,18 @@ module Gemjar
       @jar = "#{directory}/#{name}-#{version}.jar"
       @ivy = "#{directory}/ivy-#{name}-#{version}.xml"
       @pom = "#{directory}/pom-#{name}-#{version}.xml"
+    end
+
+    def open_jar &block
+      Zip::ZipFile.open(@jar, 'w', &block)
+    end
+
+    def open_ivy &block
+      File.open(@ivy, 'w+', &block)
+    end
+
+    def open_pom &block
+      File.open(@pom, 'w+', &block)
     end
   end
 end
