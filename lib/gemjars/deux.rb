8cafb07ffd2a2980afe4fd8bd123b5f7c967373c
Andrew Kiellor <akiellor@gmail.com>
2013-04-16 14:42:44 -0700

    Birthday!!!

---
+++ lib/gemjars/deux.rb
@@ -0,0 +1,7 @@
+require "gemjars/deux/version"
+
+module Gemjars
+  module Deux
+    # Your code goes here...
+  end
+end
---
+++ lib/gemjars/deux/version.rb
@@ -0,0 +1,5 @@
+module Gemjars
+  module Deux
+    VERSION = "0.0.1"
+  end
+end
