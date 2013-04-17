dfbd91c609bad18dc371a320750d1302a4c40ffb
Andrew Kiellor <akiellor@gmail.com>
2013-04-16 21:44:14 -0700

    Added ZipWriter

--- lib/gemjars/deux/zip.rb
+++ lib/gemjars/deux/zip.rb
@@ -1,6 +1,27 @@
 module Gemjars
   module Deux
+    class ZipEntry
+      CHUNK_SIZE = 2024
+      
+      attr_reader :name, :io
+      
+      def initialize name, io
+        @name = name
+        @io = io
+      end
+
+      def read
+        out = StringIO.new
+        while chunk = io.read(CHUNK_SIZE)
+          out << chunk
+        end
+        out.string
+      end
+    end
+
     class ZipReader
+      CHUNK_SIZE = 2048
+      
       include Enumerable
 
       def initialize io
@@ -9,8 +30,42 @@ module Gemjars
 
       def each &block
         while entry = @stream.next_entry
-          yield entry
+          yield ZipEntry.new(entry.name, read(entry))
         end
+      end
+
+      private
+
+      def read entry
+        tmp = Java::byte[CHUNK_SIZE].new
+        out = Java::JavaIo::ByteArrayOutputStream.new
+        while (bytes_read = @stream.read(tmp, 0, CHUNK_SIZE)) != -1
+          out.write tmp, 0, bytes_read
+        end
+
+        Java::JavaIo::ByteArrayInputStream.new(out.to_byte_array).to_io
+      end
+    end
+
+    class ZipWriter
+      CHUNK_SIZE = 2048
+      
+      def initialize io
+        @stream = Java::JavaUtilZip::ZipOutputStream.new(Java::OrgJrubyUtil::IOOutputStream.new(io))
+      end
+
+      def add_entry name, io = StringIO.new
+        @stream.put_next_entry Java::JavaUtilZip::ZipEntry.new(name)
+        while chunk = io.read(CHUNK_SIZE)
+          @stream.write chunk.to_java_bytes, 0, chunk.size
+        end
+        @stream.close_entry
+      end
+
+      def close
+        @stream.finish
+        @stream.flush
+        @stream.close
       end
     end
   end
--- spec/gemjars/deux/zip_spec.rb
+++ spec/gemjars/deux/zip_spec.rb
@@ -9,6 +9,26 @@ describe ZipReader do
   it "should yield each entry" do
     zip = ZipReader.new(File.open(zip_path))
 
-    zip.map(&:to_s).should == ["file"]
+    zip.map(&:name).should == ["file"]
+  end
+end
+
+describe ZipWriter do
+  let(:pipe) { IO.pipe }
+  let(:reader) { ZipReader.new(pipe[0]) }
+  let(:writer) { ZipWriter.new(pipe[1]) }
+
+  it "should create zip entry" do
+    writer.add_entry "file"
+    writer.close
+
+    reader.map(&:name).should == %w{file}
+  end
+
+  it "should create zip entry with content" do
+    writer.add_entry "foo", Java::JavaIo::ByteArrayInputStream.new("bar".to_java_bytes).to_io
+    writer.close
+
+    reader.map {|e| [e.name, e.read] }.should == [["foo", "bar"]]
   end
 end
