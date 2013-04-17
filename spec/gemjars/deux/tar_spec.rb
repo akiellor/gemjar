fafd6a535d5d8bf6c6c2c0a760fee74b47b13214
Andrew Kiellor <akiellor@gmail.com>
2013-04-17 09:07:24 -0700

    Initialize classpath from spec_helper.

--- spec/gemjars/deux/tar_spec.rb
+++ spec/gemjars/deux/tar_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'gemjars/deux/tar'
 
 include Gemjars::Deux
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+$CLASSPATH << Dir[File.join(File.expand_path(File.join(File.dirname(__FILE__), "..", "vendor")), "*")]
