d9d5276b5f3f21a30f5dda4e84f3f5e103adc5f8
Andrew Kiellor <akiellor@gmail.com>
2013-04-16 22:11:22 -0700

    Creates a jar with entry names.

--- lib/gemjars/deux/transform.rb
+++ lib/gemjars/deux/transform.rb
@@ -1,13 +1,38 @@
+require 'pathname'
 require 'gemjars/deux/zip'
+require 'gemjars/deux/tar'
 
 module Gemjars
   module Deux
     class Transform
-      def self.apply gem_io, jar_io
+      def self.apply name, version, gem_io, jar_io
+        new(name, version, gem_io, jar_io).apply
+     end
+
+      def initialize name, version, gem_io, jar_io
+        @name = name
+        @version = version
+        @gem_io = gem_io
+        @jar_io = jar_io
+      end
+
+      def apply
+        jar = ZipWriter.new(jar_io)
+        TarReader.new(gem_io).each do |gem_entry|
+          if gem_entry.name == "data.tar.gz"
+            TarReader.new(gem_entry.io, :gzip).each do |data_entry|
+              jar.add_entry "gems/#@name-#@version/" + data_entry.name
+            end
+          end
+        end
       ensure
         jar_io.close
         gem_io.close
       end
+
+      private
+
+      attr_reader :gem_io, :jar_io
     end
   end
 end
--- spec/gemjars/deux/transform_spec.rb
+++ spec/gemjars/deux/transform_spec.rb
@@ -1,4 +1,5 @@
 require 'gemjars/deux/transform'
+require 'set'
 
 include Gemjars::Deux
 
@@ -6,23 +7,19 @@ describe Transform do
   let(:samples) { File.expand_path("samples", File.dirname(__FILE__)) }
   let(:gem_file_path) { File.join(samples, "rspec-2.11.0.gem") }
   
-  xit "should transform a gem into a jar" do
+  it "should transform a gem into a jar" do
     gem_io = File.open(gem_file_path)
     jar_r, jar_w = IO.pipe
 
-    Transform.apply gem_io, jar_w
+    Transform.apply "rspec", "2.11.0", gem_io, jar_w
 
-    Zip.new(jar_r).map(&:to_s).should == %w{
-      bin/ruby_noexec_wrapper
-      gems/rspec-2.11.0
-      gems/rspec-2.11.0/lib
-      gems/rspec-2.11.0/lib/rspec
+    Set.new(ZipReader.new(jar_r).map(&:name)).should == Set.new(%w{
       gems/rspec-2.11.0/lib/rspec/version.rb
       gems/rspec-2.11.0/lib/rspec.rb
       gems/rspec-2.11.0/License.txt
       gems/rspec-2.11.0/README.md
-      specifications/rspec-2.11.0.gemspec
-    }
+    })
+    #specifications/rspec-2.11.0.gemspec
   end
 end
 
