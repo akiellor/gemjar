3690f38e48e60675b61d5e00b92787b171a926ef
Andrew Kiellor <akiellor@gmail.com>
2013-04-17 11:34:05 -0700

    Added a maven repository which will stream the jar and md5.

---
+++ lib/gemjars/deux/maven_repository.rb
@@ -0,0 +1,51 @@
+module Gemjars
+  module Deux
+    class MavenRepository
+      class MultiStream
+        def initialize ios
+          @ios = ios
+        end
+
+        def << chunk
+          @ios.each do |io|
+            io << chunk
+          end
+        end
+
+        def close
+          @ios.each do |io|
+            io.close
+          end
+        end
+      end
+
+      class MD5Stream
+        def initialize io
+          @io = io
+          @digest = Digest::MD5.new
+        end
+
+        def << chunk
+          @digest << chunk
+        end
+
+        def close
+          @io << @digest.hexdigest
+          @io.close
+        end
+      end
+
+      def initialize store
+        @store = store
+      end
+
+      def pipe_to name, version
+        jar_w = @store.put("org/rubygems/#{name}/#{version}/#{name}-#{version}.jar")
+        md5_w = @store.put("org/rubygems/#{name}/#{version}/#{name}-#{version}.jar.md5")
+
+        MultiStream.new([jar_w, MD5Stream.new(md5_w)])
+      end
+    end
+  end
+end
+
---
+++ spec/gemjars/deux/maven_repository_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'gemjars/deux/maven_repository'
+require 'timeout'
+
+include Gemjars::Deux
+
+RSpec::Matchers.define :be_an_io_with do |content|
+  match do |io|
+    Timeout::timeout(1) do
+      @out = StringIO.new
+      while chunk = io.read(1024)
+        @out << chunk
+      end
+      @out.string == content
+    end
+  end
+
+  failure_message_for_should do |io|
+    "expected io to have content #{content.inspect} but had #{@out.string.inspect}"
+  end
+end
+
+describe MavenRepository do
+  let(:repository) { MavenRepository.new(store) }
+  let(:store) { mock(:store) }
+
+  it "should put jar and md5 into store" do
+    jar_r, jar_w = IO.pipe 
+    md5_r, md5_w = IO.pipe 
+
+    store.stub(:put).with("org/rubygems/foo/1/foo-1.jar") { jar_w }
+    store.stub(:put).with("org/rubygems/foo/1/foo-1.jar.md5") { md5_w }
+
+    io = repository.pipe_to("foo", "1")
+
+    io << "foo\n"
+    io.close
+
+    jar_r.should be_an_io_with("foo\n")
+    md5_r.should be_an_io_with("d3b07384d113edec49eaa6238ad5ff00")
+  end
+end
