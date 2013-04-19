c2029fd8a4a51721f0ea67b50f2990a0e5a86805
Andrew Kiellor <akiellor@gmail.com>
2013-04-19 12:45:24 -0700

    Redirect tests for the http client.

--- spec/gemjars/deux/http_spec.rb
+++ spec/gemjars/deux/http_spec.rb
@@ -23,6 +23,27 @@ describe Http do
 
     r.should == "bar"
   end
+
+  ["301", "302"].each do |status|
+    it "should follow the location on a #{status}" do
+      redirect_response = mock(:response, :code => status, :header => {'location' => "www.example.com/foo"})
+      content_response = mock(:response, :code => "200")
+      content_response.should_receive(:read_body).
+        and_yield("bar")
+
+      Net::HTTP.should_receive(:get_response).
+        with(URI.parse("www.example.com")).
+        and_yield(redirect_response)
+
+      Net::HTTP.should_receive(:get_response).
+        with(URI.parse("www.example.com/foo")).
+        and_yield(content_response)
+
+      r = Streams.read(http.get("www.example.com"))
+
+      r.should == "bar"
+    end
+  end
 end
 
 
