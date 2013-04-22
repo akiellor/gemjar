4dcd3d66b97c78c2d018fc9d565e95eb3db74ca1
Andrew Kiellor <akiellor@gmail.com>
2013-04-22 12:05:44 -0700

    Pom generation creates dependency nodes.

--- lib/gemjars/deux/pom.rb
+++ lib/gemjars/deux/pom.rb
@@ -3,11 +3,15 @@ require 'builder'
 module Gemjars
   module Deux
     class Pom
+      def self.to_maven_version name, version, specs
+        specs.minimum_version name, version
+      end
+
       def initialize spec
         @spec = spec
       end
 
-      def write_to io
+      def write_to io, specs
         xml = Builder::XmlMarkup.new :target => io
         xml.instruct!
         xml.project :xmlns => "http://maven.apache.org/POM/4.0.0" do |project|
@@ -15,6 +19,16 @@ module Gemjars
           project.groupId "org.rubygems"
           project.artifactId @spec.name
           project.version @spec.version.to_s
+
+          project.dependencies do |deps_node|
+            @spec.runtime_dependencies.each do |dep|
+              deps_node.dependency do |dependency_node|
+                dependency_node.groupId "org.rubygems"
+                dependency_node.artifactId dep.name
+                dependency_node.version Pom.to_maven_version(dep.name, dep.version, specs)
+              end
+            end
+          end
         end
       end
     end
--- spec/gemjars/deux/pom_spec.rb
+++ spec/gemjars/deux/pom_spec.rb
@@ -4,7 +4,8 @@ require 'gemjars/deux/pom'
 include Gemjars::Deux
 
 describe Pom do
-  let(:spec) { mock(:spec, :name => name, :version => Gem::Version.new(version) ) }
+  let(:spec) { mock(:spec, :name => name, :version => Gem::Version.new(version), :runtime_dependencies => runtime_dependencies) }
+  let(:runtime_dependencies) { [mock(:dep, :name => "bar", :version => "2.0.0")] }
   let(:pom) { Pom.new(spec) }
   let(:samples) { File.expand_path("samples", File.dirname(__FILE__)) }
   let(:maven_schema_path) { File.join(samples, "maven-v4_0_0.xsd") }
@@ -12,14 +13,31 @@ describe Pom do
   let(:version) { "1.2.0" }
  
   it "should generate pom from spec" do
+    specs = mock(:specs)
+    specs.should_receive(:minimum_version).with("bar", "2.0.0").and_return("1.0.0")
+
     io = StringIO.new
     
-    pom.write_to io
+    pom.write_to io, specs
 
     io.string.should be_valid_xml File.read(maven_schema_path)
     io.string.should have_xpath_value "/xmlns:project/xmlns:modelVersion", "4.0.0"
     io.string.should have_xpath_value "/xmlns:project/xmlns:groupId", "org.rubygems"
     io.string.should have_xpath_value "/xmlns:project/xmlns:artifactId", name
     io.string.should have_xpath_value "/xmlns:project/xmlns:version", version
+    io.string.should have_xpath_value "//xmlns:dependency/xmlns:groupId", "org.rubygems"
+    io.string.should have_xpath_value "//xmlns:dependency/xmlns:artifactId", "bar"
+    io.string.should have_xpath_value "//xmlns:dependency/xmlns:version", "1.0.0"
+  end
+
+  context "version translation" do
+    let(:specs) { mock(:specs) }
+    it "should translate '=' version to maven style'" do
+      specs.should_receive(:minimum_version).with("rspec", "2.0.0").and_return("1.0.0")
+      
+      version = Pom.to_maven_version "rspec", "2.0.0", specs
+
+      version.should == "1.0.0"
+    end
   end
 end
