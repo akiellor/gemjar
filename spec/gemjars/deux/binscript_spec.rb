1f62925f8d8d61a21f976d5139f6af7cb3e013b4
Andrew Kiellor <akiellor@gmail.com>
2013-04-29 14:32:48 -0700

    Fixed the shebang line in the binscripts.

--- spec/gemjars/deux/binscript_spec.rb
+++ spec/gemjars/deux/binscript_spec.rb
@@ -7,6 +7,7 @@ describe Binscript do
   let(:spec) { mock(:spec, :name => "rails") }
   it "should load the right binscript for spec" do
     binscript = Binscript.new(spec, "railties")
+    binscript.to_s.should include "#!/usr/bin/env ruby"
     binscript.to_s.should include "load Gem.bin_path('rails', 'railties', version)"
   end
 end
