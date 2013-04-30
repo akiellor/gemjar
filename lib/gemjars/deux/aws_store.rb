02ea53a3b3350983e5d2d4fac57fe8b16f02bd88
Andrew Kiellor <akiellor@gmail.com>
2013-04-29 17:25:48 -0700

    Filled out the aws_store api and migrated the main binscript to use it.

--- lib/gemjars/deux/aws_store.rb
+++ lib/gemjars/deux/aws_store.rb
@@ -1,23 +1,51 @@
 require 'aws-sdk'
+require 'gemjars/deux/streams'
 
 module Gemjars
   module Deux
     class AWSStore
-      def initialize bucket
+      def self.from_file path
+        config = YAML.load(File.read(File.expand_path(path)))
+        bucket_name = config['bucket']
+        s3 = AWS::S3.new(access_key_id: config['access_key_id'], secret_access_key: config['secret_access_key'])
+        bucket = s3.buckets.create(bucket_name)
+        executor = Java::JavaUtilConcurrent::Executors.new_fixed_thread_pool(10)
+        new(bucket, executor)
+      end
+
+      def initialize bucket, executor
         @bucket = bucket
-        @threads = []
+        @executor = executor
       end
 
       def put name, opts = {}
-        r, w = IO.pipe
-        @threads << Thread.new {
-          @bucket.objects.create name, r, {:estimated_content_length => 2048 * 1024}.merge(opts)
-        }
+        r, w = Streams.pipe_channel
+        @executor.submit proc {
+          begin
+            @bucket.objects.create name, Streams.to_input_stream(r).to_io, {:estimated_content_length => 2048 * 1024}.merge(opts)
+          rescue => e
+            puts e
+          end
+        }.to_java(Java::JavaLang::Runnable)
         w
       end
 
-      def join
-        @threads.each(&:join)
+      def get name
+        return unless @bucket.objects[name].exists?
+
+        r, w = Streams.pipe_channel
+        @executor.submit proc {
+          begin
+            @bucket.objects[name].read do |chunk|
+              w.write Streams.to_buffer(chunk)
+            end
+          rescue => e
+            puts e
+          ensure
+            w.close
+          end
+        }.to_java(Java::JavaLang::Runnable)
+        r
       end
     end
   end
